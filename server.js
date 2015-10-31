8e880020cfe2e7f504fba38b2313048915d5b951
Nathan Burkiewicz <nathan.burkiewicz@gmail.com>
2015-10-31 19:29:27 +0100

    Final static todos

--- server.js
+++ server.js
@@ -23,6 +23,12 @@ app.get('/todos', function (req, res) {
     filteredTodos = _.where(filteredTodos, { completed: false })
   }
 
+  if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+    filteredTodos = _.filter(filteredTodos, function (filteredTodo) {
+      return filteredTodo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+    });
+  }
+
   res.json(filteredTodos);
 });
 
